3aa6ae9b1c30f89c9f506204fded9dfabbe98187
cgattuso <carlo.gattuso@soprasteria.com>
2024-05-26 00:06:32 +0200

    Task Scheduling

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,9 +1,12 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { Temperature } from '@prisma/client';
 import { PrismaService } from './prisma/prisma.service';
+import { Cron, CronExpression } from '@nestjs/schedule';
 
 @Injectable()
 export class AppService {
+  private readonly logger = new Logger(AppService.name);
+
   constructor(private prisma: PrismaService) {}
 
   getHello(): string {
@@ -14,13 +17,25 @@ export class AppService {
     sensorId: string;
     value: number;
   }): Promise<Temperature> {
-    return await this.prisma.temperature.create({
-      data: {
-        sensorId: userData.sensorId,
-        timestamp: new Date(),
-        txHash: '',
-        value: userData.value,
-      },
-    });
+    return await this.prisma.createMeasurement(userData);
+  }
+
+  @Cron(CronExpression.EVERY_10_SECONDS)
+  async blockchainChronicler() {
+    this.logger.verbose('Blockchain Chronicler: Start');
+
+    const nonVerifiedMeasurements: Temperature[] =
+      await this.prisma.findUnverifiedMeasurements(1);
+
+    if (nonVerifiedMeasurements) {
+      //TODO: send array of transactions to the Moonbase batch precompile
+      const txHash: string =
+        '0xa293af6faecaef71e542d78646870d20d26bacbbb5657f1231703b4a6d4c03d2';
+      await this.prisma.verifyMeasurements(nonVerifiedMeasurements, txHash);
+    }
+
+    this.logger.verbose(
+      `Blockchain Chronicler: End - ${nonVerifiedMeasurements.length} records verified`,
+    );
   }
 }

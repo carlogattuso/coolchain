206c98fcf5e053d80482163351ffebe8a5a2265e
Pablo Arribas <pablarribas@gmail.com>
2024-05-25 18:14:09 +0200

    Moonbeam service created, in progress

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,6 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { MoonbeamService } from './moonbeam.service';
 
 @Controller()
 export class AppController {
@@ -8,5 +9,15 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+}
+
+@Controller('process-data')
+export class ProcessDataController {
+  constructor(private readonly moonbeamService: MoonbeamService) {}
+
+  @Get()
+  async getHello(): Promise<string> {
+    return await this.moonbeamService.balances();
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
+import { AppController, ProcessDataController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
   imports: [],
-  controllers: [AppController],
+  controllers: [AppController, ProcessDataController],
   providers: [AppService],
 })
 export class AppModule {}

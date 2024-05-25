9163fd7fe2c2da135d69dd834a8d38790f4fd6d3
Carla Brugulat <carla@brugulat.cat>
2024-05-25 19:22:09 +0200

    database connection

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,7 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Int32 } from 'mongodb';
+import { Temperature } from '@prisma/client';
 
 @Controller()
 export class AppController {
@@ -8,5 +10,14 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Post()
+  async setTemperature(
+    @Body() userData: Temperature
+      //sensorId:number, timestamp:bigint, txHash:string, value:number
+      
+    ): Promise<Temperature>{
+    return this.appService.setTemperature(userData);
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,24 @@
 import { Injectable } from '@nestjs/common';
+import { Temperature } from '@prisma/client';
+import { PrismaService } from './prisma/prisma.service';
 
 @Injectable()
 export class AppService {
+  constructor(private prisma: PrismaService){}
   getHello(): string {
     return 'Hello World!';
+  }
+
+  async setTemperature(userData): Promise<Temperature> {
+    const result = await this.prisma.temperature.create({
+      data: {
+        sensorId: userData.sensorId,
+        timestamp: new Date(),
+        txHash: "",
+        value: userData.value,
+      }})
+      
+      
+    return result;
   }
 }

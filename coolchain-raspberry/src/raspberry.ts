c979e042f7bb651cf581dc75e5d8ddea56fb0799
carlogattuso <carlogc46@gmail.com>
2024-09-16 17:18:02 +0200

    Raspberry Pi Mock - Task scheduler + Wallet

--- coolchain-raspberry/src/raspberry.ts
+++ coolchain-raspberry/src/raspberry.ts
@@ -1,11 +1,55 @@
-import http from 'http';
+import dotenv from 'dotenv';
+import axios from 'axios';
+import {Wallet} from 'ethers';
 
-const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('¡Hola, mundo con TypeScript y ESM!');
-});
+dotenv.config();
 
-server.listen(3000, () => {
-    console.log('Servidor ejecutándose en http://localhost:3000/');
-});
+interface Record {
+  deviceId: string,
+  value: number
+}
+
+function getPrivateKey(): string {
+  const privateKey = process.env.PRIVATE_KEY;
+  if (!privateKey) {
+    throw new Error('Please provide a PRIVATE_KEY to start storing records');
+  }
+  return privateKey;
+}
+
+async function storeRecord(_address: string) {
+  const mockValue: number = Math.floor(Math.random() * 11);
+  const record: Record = {
+    deviceId: _address,
+    value: mockValue
+  }
+
+  try {
+    const response = await axios.post(`${process.env.COOLCHAIN_URL}/`, record, {
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+    console.log('Server response:', response.data);
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      console.error(`Request error: ${error.response?.status} - ${error.message}`);
+    } else {
+      console.error(`Unknown error: ${error}`);
+    }
+  }
+}
+
+async function storeTask(): Promise<void> {
+  // 1. Create wallet instance
+  const privateKey = getPrivateKey();
+  const wallet: Wallet = new Wallet(privateKey);
+
+  // 2. Call permit
+  /* TODO: Implement call permit with coolchain main wallet */
+
+  // 3. Store records
+  setInterval(() => storeRecord(wallet.address), 10000);
+}
+
+storeTask()

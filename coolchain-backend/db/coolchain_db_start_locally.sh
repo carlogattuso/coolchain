4de568d65cfdb3b56da288ada729c239cc2dd6ae
Pablo Arribas <pablarribas@gmail.com>
2024-10-05 11:22:47 +0200

    Add start db to start script

--- coolchain-backend/db/coolchain_db_start_locally.sh
+++ coolchain-backend/db/coolchain_db_start_locally.sh
@@ -1,6 +1,6 @@
 mkdir -p ~/mongodb/rs0-0 ~/mongodb/rs0-1 ~/mongodb/rs0-2
-#mongod --replSet rs0 --port 27017 --dbpath ~/mongodb/rs0-0 --bind_ip localhost --fork --logpath ~/mongodb/rs0-0/mongod.log
-#mongod --replSet rs0 --port 27018 --dbpath ~/mongodb/rs0-1 --bind_ip localhost --fork --logpath ~/mongodb/rs0-1/mongod.log
-#mongod --replSet rs0 --port 27019 --dbpath ~/mongodb/rs0-2 --bind_ip localhost --fork --logpath ~/mongodb/rs0-2/mongod.log
+mongod --replSet rs0 --port 27017 --dbpath ~/mongodb/rs0-0 --bind_ip localhost --fork --logpath ~/mongodb/rs0-0/mongod.log
+mongod --replSet rs0 --port 27018 --dbpath ~/mongodb/rs0-1 --bind_ip localhost --fork --logpath ~/mongodb/rs0-1/mongod.log
+mongod --replSet rs0 --port 27019 --dbpath ~/mongodb/rs0-2 --bind_ip localhost --fork --logpath ~/mongodb/rs0-2/mongod.log
 mongosh ./scripts/coolchain_db_start.js
 read -r -p "Press any key to continue"

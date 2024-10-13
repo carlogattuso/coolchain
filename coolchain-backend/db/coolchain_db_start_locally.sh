b146edbf4c93b488fd6db6af261a0a222bf0312d
carlogattuso <carlogc46@gmail.com>
2024-10-13 21:04:12 +0200

    Merge database scripts

--- coolchain-backend/db/coolchain_db_start_locally.sh
+++ coolchain-backend/db/coolchain_db_start_locally.sh
@@ -1,6 +1,6 @@
-mkdir -p ~/mongodb/rs0-0 ~/mongodb/rs0-1 ~/mongodb/rs0-2
-mongod --replSet rs0 --port 27017 --dbpath ~/mongodb/rs0-0 --bind_ip localhost --fork --logpath ~/mongodb/rs0-0/mongod.log
-mongod --replSet rs0 --port 27018 --dbpath ~/mongodb/rs0-1 --bind_ip localhost --fork --logpath ~/mongodb/rs0-1/mongod.log
-mongod --replSet rs0 --port 27019 --dbpath ~/mongodb/rs0-2 --bind_ip localhost --fork --logpath ~/mongodb/rs0-2/mongod.log
+mkdir -p ./data/rs0-0 ./data/rs0-1 ./data/rs0-2
+mongod --replSet rs0 --port 27017 --dbpath ./data/rs0-0 --bind_ip localhost --fork --logpath ./data/rs0-0/mongod.log
+mongod --replSet rs0 --port 27018 --dbpath ./data/rs0-1 --bind_ip localhost --fork --logpath ./data/rs0-1/mongod.log
+mongod --replSet rs0 --port 27019 --dbpath ./data/rs0-2 --bind_ip localhost --fork --logpath ./data/rs0-2/mongod.log
 mongosh ./scripts/coolchain_db_start.js
 read -r -p "Press any key to continue"

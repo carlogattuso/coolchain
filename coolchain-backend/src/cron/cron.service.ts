70343d2c1e71e5ff56908bb42920696cc7f70a0c
Pablo Arribas <pablarribas@gmail.com>
2024-10-27 21:32:23 +0100

    Minor fixes

--- coolchain-backend/src/cron/cron.service.ts
+++ coolchain-backend/src/cron/cron.service.ts
@@ -3,11 +3,10 @@ import { Cron } from '@nestjs/schedule';
 import { CreateEventDTO } from '../events/types/dto/CreateEventDTO';
 import { BlockchainService } from '../blockchain/blockchain.service';
 import { RecordsService } from '../records/records.service';
-import { MAX_RECORD_BATCH_SIZE } from '../utils/constants';
+import { EVERY_20_SECONDS, MAX_RECORD_BATCH_SIZE } from '../utils/constants';
 import { Record } from '../records/types/Record';
 import { Event } from '../events/types/Event';
 import { EventsService } from '../events/events.service';
-import { CronExpression } from '@nestjs/schedule/dist/enums/cron-expression.enum';
 
 @Injectable()
 export class CronService {
@@ -19,7 +18,7 @@ export class CronService {
     private readonly _blockchainService: BlockchainService,
   ) {}
 
-  @Cron(CronExpression.EVERY_10_SECONDS)
+  @Cron(EVERY_20_SECONDS)
   async blockchainChronicler() {
     this.logger.verbose('Blockchain Chronicler: Start');
 
